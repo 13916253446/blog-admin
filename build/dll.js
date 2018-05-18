eabb7c145d34a86e3fb94633132d79df32861ed0
崔海峰 <cuihaifeng1919@hotmail.com>
2018-05-18 11:06:08 +0800

    优化webpack

--- build/dll.js
+++ build/dll.js
@@ -5,10 +5,3 @@ import Vuex from 'vuex';
 import VueRouter from 'vue-router';
 
 import axios from 'axios';
-
-//  解决移动端点击延迟
-import fastclick from 'fastclick';
-
-//  vw兼容方案
-import viewportUnitsBuggyfill from 'viewport-units-buggyfill';
-import viewportUnitsBuggyfillHacks from 'viewport-units-buggyfill/viewport-units-buggyfill.hacks.js';
